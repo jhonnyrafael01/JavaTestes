33ada4e726aa2dfa5df09ba8fc815995849f8342
jhonnyrafael01 <rafaeljhonny01@gmail.com>
2022-01-09 13:55:31 -0300

    aplicando modificadores de acesso e resolução do desafio mdAcesso

---
+++ src/oo/encapsulamento/casaA/Ana.java
@@ -0,0 +1,13 @@
+package oo.encapsulamento.casaA;
+
+public class Ana {
+	
+	@SuppressWarnings("unused")
+	private String segredo = "...";
+	String facoDentroDeCasa = "..."; //deafault ou pacote
+	protected String formaDeFalar = "...";
+	public String todosSabem = "...";
+	
+	
+
+}
--- src/oo/heranca/teste/CarroTeste.java
+++ src/oo/heranca/teste/CarroTeste.java
@@ -1,10 +1,14 @@
-package oo.heranca.desafio;
+package oo.heranca.teste;
+
+import oo.heranca.desafio.Carro;
+import oo.heranca.desafio.Civic;
+import oo.heranca.desafio.Ferrari;
 
 public class CarroTeste {
 
 	public static void main(String[] args) {
 
-		Carro c1 = new Civic();
+		Civic c1 = new Civic();
 
 		c1.acelerar();
 		System.out.println(c1);
