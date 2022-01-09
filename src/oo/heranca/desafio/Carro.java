015c4a25e191ff12c8d6b2ea2e4f2a9029127618
jhonnyrafael01 <rafaeljhonny01@gmail.com>
2022-01-09 10:05:21 -0300

    atualização do pacote herança e desafio

--- src/oo/heranca/desafio/Carro.java
+++ src/oo/heranca/desafio/Carro.java
@@ -2,17 +2,27 @@ package oo.heranca.desafio;
 
 public class Carro {
 
+	final int VELOCIDADE_MAXIMA;
 	int velocidadeAtual;
+	int delta = 5;
+
+	Carro(int velocidadeMaxima) {
+		VELOCIDADE_MAXIMA = velocidadeMaxima;
+	}
 
 	void acelerar() {
-		velocidadeAtual += 5;
+		if (velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
+			velocidadeAtual = VELOCIDADE_MAXIMA;
+		} else {
+			velocidadeAtual += delta;
+		}
 	}
 
 	void frear() {
-		if (this.velocidadeAtual <= 0) {
-			System.out.println("Carro parado");
+		if (velocidadeAtual >= 5) {
+			velocidadeAtual -= 5;
 		} else {
-			this.velocidadeAtual -= 5;
+			velocidadeAtual = 0;
 		}
 	}
 
