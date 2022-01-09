1b9bd73f88c20a80cbc978eb9b1f39360832f2ca
jhonnyrafael01 <rafaeljhonny01@gmail.com>
2022-01-09 15:18:20 -0300

    testando polimofismo dinamico

---
+++ src/oo/polimofismo/Arroz.java
@@ -0,0 +1,11 @@
+package oo.polimofismo;
+
+public class Arroz extends Comida{
+	
+	
+	public Arroz(double peso) {
+		super(peso);
+	}
+
+	
+}
---
+++ src/oo/polimofismo/Feijao.java
@@ -0,0 +1,8 @@
+package oo.polimofismo;
+
+public class Feijao extends Comida{
+	
+	public Feijao(double peso) {
+		super(peso);
+	}
+}
