fc36c0496bde90348aadda340d787c0f752ea74c
jhonnyrafael01 <rafaeljhonny01@gmail.com>
2022-01-12 20:31:04 -0300

    Aprendendo sobre lambdas e interfaces funcionais

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,6 +1,15 @@
 package lambdas;
 
+@FunctionalInterface //usada para força a ter apenas um unico método dentro da interface
 public interface Calculo {
 	
 	double executar(double a, double b);
+	
+	default String legal() {
+		return "Legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito lagal";
+	}
 }
--- src/lambdas/CalculoTeste2.java
+++ src/lambdas/CalculoTeste2.java
@@ -9,5 +9,13 @@ public class CalculoTeste2 {
 
 		calc = (x, y) -> x * y;
 		System.out.println(calc.executar(2, 3));
+		
+		System.out.println(calc.legal());
+		System.out.println(Calculo.muitoLegal());
 	}
 }
+
+/* Usando a funcção lambda se torna mais 'simples' o código, pois não precisa criar uma classe
+ * e implementar a logica nela para depois estaciala em uma outra classe
+ * */
+ 
